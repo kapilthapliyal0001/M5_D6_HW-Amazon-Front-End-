9e51eb2691e3c7e5401be7719b9317877845b902
Kapil Thapliyal <kapilthapliyal0001@gmail.com>
2021-06-28 15:00:23 +0200

    React Front end start

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* ------------ */}
+      {/* Put here your component */}
+    </div>
+  );
+}
+
+export default App;
